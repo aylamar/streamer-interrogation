a78956d94241c8fbfe02608e982cb6c051a1f40c
aylamar <aylamar@users.noreply.github.com>
2021-12-30 17:15:36 -0600

    feat: very basic authentication

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -4,6 +4,24 @@ import path from "path";
 const router = express.Router();
 
 router.get("/", (req, res) => {
+    const reject = () => {
+        res.setHeader('www-authenticate', 'Basic')
+        res.sendStatus(401)
+    }
+
+    const authorization = req.headers.authorization
+
+    if(!authorization) {
+        return reject()
+    }
+
+    const [username, password] = Buffer.from(authorization.replace('Basic ', ''), 'base64').toString().split(':')
+
+    let user = process.env.LOGIN_USER
+    let pass = process.env.LOGIN_PASS
+
+    if(! (username === user && password === pass)) return reject()
+
     res.sendFile(path.join(__dirname, "../../public/admin.html"));
 });
 
